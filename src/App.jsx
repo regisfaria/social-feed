dc9e93d663650a51f4e2bf43727142352e79cd35
regisfaria <regisprogramming@gmail.com>
2022-07-04 23:08:23 +0200

    added Header to app, started structuring mainc ontent

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Header } from "./components/Header";
 
+import styles from "./App.module.css";
 import "./global.css";
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <h1>Hello World</h1>
-    </>
+
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+
+        <main>
+          <h1>content</h1>
+        </main>
+      </div>
+    </div>
   );
 }
 
