cd4cc722d22616b9d9c7b725146b52e618947d21
regisfaria <regisprogramming@gmail.com>
2022-07-04 23:52:18 +0200

    added Sidebar component and styled a little bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
@@ -10,7 +11,7 @@ function App() {
       <Header />
 
       <div className={styles.wrapper}>
-        <aside>sidebar</aside>
+        <Sidebar />
 
         <main>
           <Post
