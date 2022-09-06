09190186552eb4ed820696197b1ec32321ef4dc7
regisfaria <regisprogramming@gmail.com>
2022-09-06 15:54:44 +0200

    added post mock data

---
+++ src/mocks/posts.js
@@ -0,0 +1,27 @@
+export const postData = {
+  first: {
+    content: [
+      "Loving this network so far!",
+      "Yesterday I've been into an event and share this for everyone there.",
+      "I hope we have more devs in time! 😍",
+      "Below I'm leaving my GitHub so you guys can check my work! ✌️🫡",
+    ],
+    embedded: {
+      uri: "https://github.com/regisfaria",
+      text: "My GitHub Profile",
+    },
+    hashtags: ["#project", "#devsUnited", "#socialNetwork", "#gitHub"],
+  },
+  second: {
+    content: [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      "Excepturi officiis veritatis esse non? Ducimus reiciendis facilis voluptatem nisi sequi omnis repellendus vel possimus.",
+      "Earum odit eius suscipit excepturi deleniti iure!",
+    ],
+    embedded: {
+      uri: "https://google.com",
+      text: "Google Search Engine",
+    },
+    hashtags: ["#lorem", "#ipsum", "#randomTxt"],
+  },
+};
