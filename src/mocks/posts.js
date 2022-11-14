ddabf37c1b3e2a73abcdc65e4e517edcdd89649b
regisfaria <regisprogramming@gmail.com>
2022-11-14 23:30:18 +0100

    reworked post data

--- src/mocks/posts.js
+++ src/mocks/posts.js
@@ -1,27 +1,40 @@
-export const postData = {
-  first: {
+export const posts = [
+  {
+    id: 1,
+    author: {
+      name: "Régis Faria",
+      profileUrl: "https://github.com/regisfaria.png",
+      role: "Developer",
+    },
     content: [
-      'Loving this network so far!',
+      "Loving this network so far!",
       "Yesterday I've been into an event and share this for everyone there.",
-      'I hope we have more devs in time! 😍',
+      "I hope we have more devs in time! 😍",
       "Below I'm leaving my GitHub so you guys can check my work! ✌️🫡",
     ],
     embedded: {
-      uri: 'https://github.com/regisfaria',
-      text: 'My GitHub Profile',
+      uri: "https://github.com/regisfaria",
+      text: "My GitHub Profile",
     },
-    hashtags: ['#project', '#devsUnited', '#socialNetwork', '#gitHub'],
+    hashtags: ["#project", "#devsUnited", "#socialNetwork", "#gitHub"],
   },
-  second: {
+  {
+    id: 2,
+    author: {
+      name: "John Doe",
+      profileUrl:
+        "https://xesque.rocketseat.dev/users/avatar/profile-7a16c64f-e5c4-418f-8530-089d830e9ade-1628641872955.jpg",
+      role: "Project Manager",
+    },
     content: [
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      'Excepturi officiis veritatis esse non? Ducimus reiciendis facilis voluptatem nisi sequi omnis repellendus vel possimus.',
-      'Earum odit eius suscipit excepturi deleniti iure!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      "Excepturi officiis veritatis esse non? Ducimus reiciendis facilis voluptatem nisi sequi omnis repellendus vel possimus.",
+      "Earum odit eius suscipit excepturi deleniti iure!",
     ],
     embedded: {
-      uri: 'https://google.com',
-      text: 'Google Search Engine',
+      uri: "https://google.com",
+      text: "Google Search Engine",
     },
-    hashtags: ['#lorem', '#ipsum', '#randomTxt'],
+    hashtags: ["#lorem", "#ipsum", "#randomTxt"],
   },
-}
+];
